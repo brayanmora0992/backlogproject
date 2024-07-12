d9b983f17db92bfa97874552084bcc6368e26565
MORA Brayan <brayanmora0992@gmail.com>
2024-07-12 15:20:26 -0300

    se agregó la creación de la tabla provisoria

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,21 @@ function processSearch() {
     console.log(gametitle);
     //define una variable para que se abra la página de hltb con la búsqueda
     let howlongtobeat = window.open('https://howlongtobeat.com/?q='+gametitle)
+}
 
+function createTableHeaders() {
+    let tableHead = document.getElementById('backlog').querySelector('thead');
+    tableHead.innerHTML = '';
 
+    //Crea los elementos th y los agrega al thead
+    let headerRow = document.createElement('tr');
+    let headers = ['Title', 'Platform', 'Total Time'];
+
+    headers.forEach(headerText => {
+      let th = document.createElement('th');
+      th.textContent = headerText;
+      headerRow.appendChild(th);
+    });    
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -25,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let formData = new FormData(form);
 
         //Envia los datos a Google usando fetch
-        fetch('https://script.google.com/macros/s/AKfycbxcUdUr1yyoIqJPk2WOmtwwSMo4n8I03wKaeo-HrGM2Xsr-Y5zmNxGdUP2pR14sM6wqYQ/exec', {
+        fetch('https://script.google.com/macros/s/AKfycbxrhL7un2AE62e5r1NEbUNctgTtEPZHeu4IR3mnBW5ERD349KYWV63SOhAR8XImxafmlg/exec', {
             method: 'POST',
             body: formData
         })
@@ -47,11 +60,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.getElementById('viewBacklog').addEventListener('click', ()=>{
-        fetch('https://script.google.com/macros/s/AKfycbxcUdUr1yyoIqJPk2WOmtwwSMo4n8I03wKaeo-HrGM2Xsr-Y5zmNxGdUP2pR14sM6wqYQ/exec')
+        //crea la fila de encabezado de la tabla
+        createTableHeaders();
+
+        fetch('https://script.google.com/macros/s/AKfycbxrhL7un2AE62e5r1NEbUNctgTtEPZHeu4IR3mnBW5ERD349KYWV63SOhAR8XImxafmlg/exec')
         .then(response => response.json())
         .then(data => {
           // Aquí puedes procesar los datos recibidos, por ejemplo, mostrarlos en tu página
-          console.log(data); // Muestra los datos en la consola para verificar
+          console.log(data);
+          
+          let tableBody = document.getElementById('backlog').getElementsByTagName('tbody')[0];
+          data.forEach(item => {
+              let row = tableBody.insertRow();
+              let cell1 = row.insertCell(0);
+              let cell2 = row.insertCell(1);
+              let cell3 = row.insertCell(2);
+              
+              cell1.textContent = item.gametitle;
+              cell2.textContent = item.platform;
+              cell3.textContent = item.totaltime;
+          });
         })
         .catch(error => {
           console.error('Error al obtener los datos:', error);
